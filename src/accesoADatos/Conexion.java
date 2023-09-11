e7d5388dddbd007bedadb6812e0db38a4eceae5f
Laboratorio-01 <gnulinuxoffline@gmail.com>
2023-09-11 16:24:51 -0300

    Se agregan clases esenciales

---
+++ src/accesoADatos/Conexion.java
@@ -0,0 +1,34 @@
+
+package accesoADatos;
+
+import java.sql.Connection;
+import java.sql.*;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+ 
+    private static Connection conexion=null;
+    private static final String URL="jdbc.mariadb://localhost/ULPUniversity";
+    private static final String USER="root";
+    private static final String PASS="";
+
+    private Conexion() {
+    }
+    
+    public static Connection obtenerConexion(){
+        if (conexion!=null) {
+            try {
+                conexion=DriverManager.getConnection(URL, USER, PASS);
+                System.out.println("Conectado!!!");
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error de Conexión: "+ex.getMessage());
+            }
+        }
+        return conexion;
+    }
+    
+    
+}
---
+++ src/universidadgrupo26/Main.java
@@ -0,0 +1,15 @@
+package universidadgrupo26;
+
+import accesoADatos.Conexion;
+import java.sql.Connection;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+     
+        Connection conn = Conexion.obtenerConexion();
+
+    }
+
+}
